019f661f054e4c40b1670de9825a177980a6323e
Ibrahima SOURABIE <ge65yub@mytum.de>
2025-05-11 21:57:29 +0200

    Adding Header

---
+++ src/pages/coaches/CoachRegister.vue
@@ -0,0 +1,3 @@
+<template>
+  <section>Reister</section>
+</template>
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,18 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
+import NotFound from "../pages/NotFound.vue";
+import CoachesList from "../pages/coaches/CoachesList.vue";
+import CoachRegister from "../pages/coaches/CoachRegister.vue";
+import RequestsReceived from "../pages/requests/RequestsReceived.vue";
+import ContactCoach from "../pages/requests/ContactCoach.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
-    { path: "/coaches", component: null },
+    { path: "/coaches", component: CoachesList },
     {
       path: "/coaches/:id",
       component: null,
-      children: [{ path: "contact", component: null }],
+      children: [{ path: "contact", component: ContactCoach }],
     },
-    { path: "/register", component: null },
-    { path: "/requests", component: null },
-    { path: "/:notFound(.*)", component: null },
+    { path: "/register", component: CoachRegister },
+    { path: "/requests", component: RequestsReceived },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
 
